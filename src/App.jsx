56d26dafe690f42a64ba58ea5829f8c0a80b99ca
Canu Fabien <fabien.canu@gmail.com>
2023-06-20 11:34:40 +0200

    Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Spinner, Stack } from "react-bootstrap";
-
 import "./App.css";
 
-import Home from "./pages/Home";
-import List from "./pages/List";
+import { useEffect, useState } from "react";
 
 function App() {
+  // Définiton des variable d'état local (useState())
   const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState("home");
   const [seismes, setSeismes] = useState(null);
 
+  // Définition de l'effet qui s'execute uniquement au chargement du composant
   useEffect(() => {
-    const url = "https://isenapi.koality.pw/api/seismes/50000";
+    const url = "https://isenapi.koality.pw/api/seismes/5000";
     const fetchData = async () => {
       try {
         const response = await fetch(url);
@@ -26,28 +23,41 @@ function App() {
     };
 
     fetchData();
-  }, []);
+  }, []); // <- c'est le tableau vide qui permet de définir que cet effet ne se declanche qu'une fois
 
-  const changePage = (page) => {
-    window.location.href = "#";
-    setPage(page);
-  };
+  // rendu
+  if (loading) return <p>Chargement ...</p>;
+  return (
+    <div className="p-4">
+      {seismes &&
+        seismes.map((seisme, i) => (
+          <div key={`seisme-${i}`} className="col">
+            <div className="card shadow-sm mb-4">
 
-  if (loading)
-    return (
-      <Stack>
-        <Spinner
-          animation="border"
-          variant="primary"
-          className="mx-auto mt-5"
-        />
-      </Stack>
-    );
-  if (page === "home") {
-    return <Home seismes={seismes} changePage={changePage}></Home>;
-  } else if (page === "list") {
-    return <List seismes={seismes} changePage={changePage}></List>;
-  }
+              <div className="card-body">
+                <p className="card-text">
+                  {seisme.pays}
+                  <span
+                    className="float-end rounded-circle"
+                    style={{
+                      height: "20px",
+                      width: "20px",
+                      background: "gray",
+                    }}
+                  ></span>
+                </p>
+                <div className="d-flex justify-content-between align-items-center">
+                  <small className="text-muted">
+                    {new Date(seisme.instant).toLocaleDateString("fr")}
+                  </small>
+                  <small className="text-muted">Mag. {seisme.mag}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default App;
